8cc5f03d78d80f5d1893a4268924a78075c02023
maaaajid <bouaamiabdelmajid@gmail.com>
2024-08-01 11:14:42 +0100

    added clien class

--- includes/client.hpp
+++ includes/client.hpp
@@ -6,13 +6,25 @@
 class client
 {
     private :
-        int     c_socket; //client socket
-        std::string  nickname;
-        std::string  username;
-        std::string  c_state; // client connection state
+        int     c_fd; //client socket
+        string  nickname;
+        string  username;
+        string  c_ip;
+        string  c_state; // client connection state
 
     public :
         client(){};
+        ~client(){};
+        void            setC_fd(int fd){this->c_fd = fd;};
+        void            setNickname(string nickName){this->nickname = nickName;};
+        void            setUsername(string userName){this->username = userName;};
+        void            setC_ip(string c_Ip){this->c_ip = c_Ip;};
+        void            setC_state(string c_State){this->c_state = c_State;};
+        int             getC_fd(void){return(this->c_fd);};
+        string     getnickName(void){return (this->nickname);};
+        string     getuserName(void){return (this->username);};
+        string     getC_ip(void){return (this->c_ip);};
+        string     getC_state(void){return (this->c_state);};
 
 };
 
